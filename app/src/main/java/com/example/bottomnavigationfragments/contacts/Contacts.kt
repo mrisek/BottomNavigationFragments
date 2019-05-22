39e7167c4f1fc6c574c33a164d50a9136446a9e8
Matija Risek <mrisek@p8.team>
2019-05-22 12:14:31 +0200

    setup fragment & activity routing, add navigation destinations, add icons

--- app/src/main/java/com/example/bottomnavigationfragments/contacts/Contacts.kt
+++ app/src/main/java/com/example/bottomnavigationfragments/contacts/Contacts.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 import com.example.bottomnavigationfragments.R
 
@@ -14,8 +16,17 @@ class Contacts : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false)
+        val view= inflater.inflate(R.layout.fragment_contacts, container,false)
+
+        view.findViewById<Button>(R.id.example_activity_btn).setOnClickListener {
+            findNavController().navigate(R.id.action_nav_contacts_to_exampleActivity)
+        }
+
+        view.findViewById<Button>(R.id.example_fragment_btn).setOnClickListener {
+            findNavController().navigate(R.id.action_nav_contacts_to_exampleFragment)
+        }
+
+        return view
     }
 
 }
---
+++ app/src/main/java/com/example/bottomnavigationfragments/exampleActivity/ExampleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bottomnavigationfragments.exampleActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bottomnavigationfragments.R
+
+class ExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+    }
+}
